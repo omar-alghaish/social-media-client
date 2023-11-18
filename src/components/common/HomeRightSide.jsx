0fbf5a213f4ff7da9ac1fac50eb272f2e6869ea5
omar-alghaish <omaralghish@gmail.com>
2023-11-18 23:05:35 +0200

    some changes

--- src/components/common/HomeRightSide.jsx
+++ src/components/common/HomeRightSide.jsx
@@ -1,10 +1,57 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import { Divider, Typography } from "@mui/material";
+import { Box, Stack, useTheme } from "@mui/system";
+import React from "react";
 
 const HomeRightSide = () => {
-  return (
-    <Box className="right-side-home-page">HomeRightSide</Box>
-  )
-}
+  const theme = useTheme();
 
-export default HomeRightSide
+  return (
+    <Box
+      className="right-side-home-page"
+      sx={{
+        padding: "20px",
+        paddingBottom: "20px",
+        background: theme.palette.background.paper,
+        borderRadius: "20px",
+      }}
+    >
+      <Typography fontSize="20px">Trend for you</Typography>
+      <Typography
+        textTransform="uppercase"
+        sx={{
+          color: "text.secondary",
+          mt: "20px",
+          mb: "10px",
+          fontSize: "13px",
+        }}
+        variant="body2"
+      >
+        trending in egypt
+      </Typography>
+      <Stack>
+      <Box sx={{p:"10px"}}>
+          <Typography>#web</Typography>
+          <Typography
+            sx={{ color: "text.secondary", fontSize: "13px" }}
+            variant="body2"
+          >
+            26.2k Post
+          </Typography>
+          <Divider sx={{p:"5px"}}/>
+        </Box>
+        <Box sx={{p:"10px"}}>
+          <Typography>#ReactJs</Typography>
+          <Typography
+            sx={{ color: "text.secondary", fontSize: "13px" }}
+            variant="body2"
+          >
+            26.2k Post
+          </Typography>
+          <Divider sx={{p:"5px"}}/>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default HomeRightSide;
